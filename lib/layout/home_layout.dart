f56b57941296aafb77a7244179e08e7ab6b20a1c
MahmoudAlsayes <mahmoudalsayes03@gmail.com>
2023-10-08 23:55:21 +0300

    init

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  static const String routeName = "hmoe";
+  const HomeLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: 
+        Text("Todo"),
+      ),
+    );
+  }
+}
